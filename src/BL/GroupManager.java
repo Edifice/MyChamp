f969dd02b08254ff6cd9e25e388a52f0242ed760
Anthony Monori <anthonymonori@gmail.com>
2013-01-09 20:55:55 +0100

    BL got updated with cleaner code and basic javaDOC was added.

--- src/BL/GroupManager.java
+++ src/BL/GroupManager.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 public class GroupManager {
 
     private GroupDBManager DBM;
+    private TeamManager TM;
 
     public GroupManager() throws SQLException {
         DBM = new GroupDBManager();
@@ -21,23 +22,34 @@ public class GroupManager {
     public void addGroup(Group group) throws SQLException {
         DBM.addGroup(group);
     }
-    
+
     public void updateGroup(Group group) throws SQLException {
         DBM.updateGroup(group);
     }
-    
+
     public void removeById(int iden) throws SQLException {
         DBM.removeById(iden);
     }
 
+    /**
+     * Groups the teams into four different groups assigning them the ID's from
+     * 1 to 4 until every team has a group assigned to it. It uses the
+     * assignToGroup method which takes in a team and a group id. All the groups
+     * have at least 3, but maximum 4 teams.
+     *
+     * @throws SQLException
+     */
     public void groupTeams() throws SQLException {
-        TeamManager TM = new TeamManager();
+        TM = new TeamManager();
         ArrayList<Team> allTeams = TM.getAll();
         int i = 1;
-        int size = allTeams.size();
         for (Team team : allTeams) {
             TM.assignToGroup(team, i);
-            if (i / 4 == 1) {i = 1;} else {i++;}
+            if (i / 4 == 1) {
+                i = 1;
+            } else {
+                i++;
+            }
         }
     }
 }
--- src/BL/MatchManager.java
+++ src/BL/MatchManager.java
@@ -18,7 +18,15 @@ public class MatchManager {
     public MatchManager() throws SQLException {
         DBM = new MatchDBManager();
     }
-    
+
+    /**
+     * Start the tournament.
+     *
+     * Gets all the teams inside a group and schedules the matched by calling
+     * for the groupTeams() and generateMatchesByGroup(group) methods.
+     *
+     * @throws SQLException
+     */
     public void startTournament() throws SQLException {
         GM = new GroupManager();
         GM.groupTeams();
@@ -27,54 +35,82 @@ public class MatchManager {
             generateMatchesByGroup(group);
         }
     }
-    
-    public void createNewMatch(int round, Team homeTeam, Team guestTeam) throws SQLException{
+
+    /**
+     * Creates a match.
+     *
+     * Creates a match and sends in to the database.
+     *
+     * @param round the round of the game
+     * @param homeTeam the home team
+     * @param guestTeam the guest team
+     * @throws SQLException
+     */
+    public void createNewMatch(int round, Team homeTeam, Team guestTeam) throws SQLException {
         Match newMatch = new Match(round, homeTeam.getID(), guestTeam.getID());
         DBM.addMatch(newMatch);
     }
-    
+
     public ArrayList<Match> getAll() throws SQLException {
         return DBM.getAll();
     }
-    
-    public void updateScore(Match match) throws SQLException{
+
+    public void updateScore(Match match) throws SQLException {
         DBM.updateScore(match);
     }
-    
+
     public void updateMatch(Match match) throws SQLException {
         DBM.updateMatch(match);
     }
-    
+
+    /**
+     * Schedules the matches by group.
+     *
+     * This method uses the Round-robin tournament algorithm (see:
+     * http://en.wikipedia.org/wiki/Round-robin_tournament) to generate the
+     * matches in each group separately dividing them into rounds. Every team
+     * meets the other teams in the group twice (once home once away) but do not
+     * meet each other twice in consecutive rounds. In case where there are only
+     * 3 team (odd number) in a group a dummy team is created but no matches are
+     * generated for them, meaning the team who plays the dummy team stays for a
+     * round. We store the teams inside an ArrayList where the first team is
+     * fixed and the three others are rotated. For more information about the
+     * algorithm please look for the source.
+     *
+     * @param group the group where the matches are generated
+     * @throws SQLException
+     * @see http://en.wikipedia.org/wiki/Round-robin_tournament
+     */
     public void generateMatchesByGroup(Group group) throws SQLException {
         TM = new TeamDBManager();
         ArrayList<Team> teams = TM.getTeamsByGroup(group.getID());
-        
+
         //Adds the dummy team.
         Team dummy = new Team();
         if (teams.size() % 2 != 0) {
             teams.add(dummy);
         }
-        
+
         int rounds = teams.size() - 1;
-        
+
         Team fixed = teams.get(0);
         teams.remove(0);
-        
-        for (int i=1; i <= rounds; i++) {
+
+        for (int i = 1; i <= rounds; i++) {
             Team second = teams.get(0);
             Team third = teams.get(1);
             Team fourth = teams.get(2);
-            
-            if (!second.equals(dummy)){
+
+            if (!second.equals(dummy)) {
                 createNewMatch(i, fixed, second);
-                createNewMatch(i+rounds, second, fixed);
+                createNewMatch(i + rounds, second, fixed);
             }
-            
+
             if (!third.equals(dummy) && !fourth.equals(dummy)) {
                 createNewMatch(i, third, fourth);
-                createNewMatch(i+rounds, fourth, third);
+                createNewMatch(i + rounds, fourth, third);
             }
-            
+
             Collections.rotate(teams, 1);
         }
     }
