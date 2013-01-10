a58ffb62c56dd6302e157d14466909aec0e80360
Martin Rasmussen <martinrasmussen92@gmail.com>
2013-01-10 11:17:05 +0100

    Added a getMatchesByGroup(Group g) Method

--- src/DAL/MatchDBManager.java
+++ src/DAL/MatchDBManager.java
@@ -1,5 +1,6 @@
 package DAL;
 
+import BE.Group;
 import BE.Match;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -8,53 +9,53 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class MatchDBManager extends DBManager {
-    
-    
+
     public MatchDBManager() throws SQLException {
         super();
     }
+
     public void addMatch(Match match) throws SQLException {
         Connection con = dS.getConnection();
         PreparedStatement qMatch = con.prepareStatement("INSERT INTO Match VALUES (?, ?, ?, ?, ?, ?)");
 
-        
+
         qMatch.setInt(1, match.getRound());
         qMatch.setInt(2, match.getHomeTeamID());
         qMatch.setInt(3, match.getGuestTeamID());
         qMatch.setInt(4, match.getIsPlayed());
         qMatch.setNull(5, java.sql.Types.INTEGER);
         qMatch.setNull(6, java.sql.Types.INTEGER);
-                
+
         qMatch.executeUpdate();
 
         con.close();
     }
-/*
-    public void updateMatch(Match match) throws SQLException {
-        Connection con = dS.getConnection();
+    /*
+     public void updateMatch(Match match) throws SQLException {
+     Connection con = dS.getConnection();
 
-        PreparedStatement qTeam = con.prepareStatement("UPDATE Match SET  Round = ?, HomeTeamID = ?, GuestTeamID = ?, isPlayed = ? WHERE ID = ?");
+     PreparedStatement qTeam = con.prepareStatement("UPDATE Match SET  Round = ?, HomeTeamID = ?, GuestTeamID = ?, isPlayed = ? WHERE ID = ?");
 
-        qTeam.setInt(1, match.getRound());
-        qTeam.setInt(2, match.getHomeTeamID());
-        qTeam.setInt(3, match.getGuestTeamID());
-        qTeam.setInt(4, match.isIsPlayed());
+     qTeam.setInt(1, match.getRound());
+     qTeam.setInt(2, match.getHomeTeamID());
+     qTeam.setInt(3, match.getGuestTeamID());
+     qTeam.setInt(4, match.isIsPlayed());
 
-        qTeam.setInt(5, match.getID());
+     qTeam.setInt(5, match.getID());
 
-        qTeam.executeUpdate();
+     qTeam.executeUpdate();
 
-        con.close();
-    }*/
-    
-    public void updateScore(Match match) throws SQLException{
+     con.close();
+     }*/
+
+    public void updateScore(Match match) throws SQLException {
         Connection con = dS.getConnection();
 
         PreparedStatement qTeam = con.prepareStatement("UPDATE Match SET  HomeGoals = ?, GuestGoals = ? WHERE ID = ?");
 
-        
+
         qTeam.setInt(1, match.getHomeGoals());
-        qTeam.setInt(2, match.getGuestGoals());        
+        qTeam.setInt(2, match.getGuestGoals());
 
         qTeam.setInt(3, match.getID());
 
@@ -62,6 +63,7 @@ public class MatchDBManager extends DBManager {
 
         con.close();
     }
+
     @Override
     public void removeById(int iden) throws SQLException {
         Connection con = dS.getConnection();
@@ -72,7 +74,7 @@ public class MatchDBManager extends DBManager {
 
         con.close();
     }
-    
+
     public Match getMatchById(int id) throws SQLException {
         Connection con = dS.getConnection();
         Match match;
@@ -83,14 +85,14 @@ public class MatchDBManager extends DBManager {
 
         allMatches.next();
         match = new Match(
-                    allMatches.getInt("ID"),
-                    allMatches.getInt("MatchRound"),
-                    allMatches.getInt("HomeTeamID"),
-                    allMatches.getInt("GuestTeamID"),
-                    allMatches.getInt("IsPlayed"),
-                    allMatches.getInt("HomeGoals"),
-                    allMatches.getInt("GuestGoals"));
-        
+                allMatches.getInt("ID"),
+                allMatches.getInt("MatchRound"),
+                allMatches.getInt("HomeTeamID"),
+                allMatches.getInt("GuestTeamID"),
+                allMatches.getInt("IsPlayed"),
+                allMatches.getInt("HomeGoals"),
+                allMatches.getInt("GuestGoals"));
+
         con.close();
         return match;
 
@@ -123,13 +125,37 @@ public class MatchDBManager extends DBManager {
 
     }
 
+    public ArrayList<Match> getMatchesByGroup(Group g) throws SQLException {
+        Connection con = dS.getConnection();
+        ArrayList<Match> matches = new ArrayList<>();
+
+        PreparedStatement qAllMatches = con.prepareStatement("SELECT Match.* FROM Match INNER JOIN Team ON Match.HomeTeamID = Team.ID WHERE Team.GroupID = ?");
+        qAllMatches.setInt(1, g.getID());
+        ResultSet allMatches = qAllMatches.executeQuery();
+
+        while (allMatches.next()) {
+            matches.add(
+                    new Match(
+                    allMatches.getInt("ID"),
+                    allMatches.getInt("MatchRound"),
+                    allMatches.getInt("HomeTeamID"),
+                    allMatches.getInt("GuestTeamID"),
+                    allMatches.getInt("IsPlayed"),
+                    allMatches.getInt("HomeGoals"),
+                    allMatches.getInt("GuestGoals")));
+        }
+
+        con.close();
+        return matches;
+
+    }
+
     @Override
     public void removeAll() throws SQLException {
         Connection con = dS.getConnection();
         PreparedStatement qData = con.prepareStatement("DELETE FROM Match; DBCC CHECKIDENT (Match, RESEED, 0)");
         qData.executeUpdate();
-                
+
         con.close();
     }
-
 }
