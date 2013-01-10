c64e9a1d52fb693cd8148b15677f81906f72637b
Anthony Monori <anthonymonori@gmail.com>
2013-01-10 11:51:45 +0100

    Playing around with rankings.

--- src/BL/RankManager.java
+++ src/BL/RankManager.java
@@ -12,16 +12,28 @@ public class RankManager {
 
     private TeamDBManager TM;
     private MatchDBManager MM;
-    
+
     public RankManager() {
-        
     }
-    
+
     public void calculateTotalGoals(Group group) throws SQLException {
         ArrayList<Team> teams = TM.getTeamsByGroup(group);
         ArrayList<Match> matches = MM.getMatchesByGroup(group);
+        
+        ArrayList<Integer> totalgoalsofteams = new ArrayList<>();
+        for (Team team : teams) {
+            int totalGoals = 0;
+            for (Match match : matches) {
+                if (team.getID() == match.getHomeTeamID()) {
+                    totalGoals += match.getHomeGoals();
+                } else if (team.getID() == match.getGuestTeamID()) {
+                    totalGoals += match.getGuestGoals();
+                }
+            }
+            totalgoalsofteams.add(totalGoals);
+        }
     }
-    
+
     public ArrayList<Team> getRankingByPoints(Group group) throws SQLException {
         return TM.getTeamByPoints(group);
     }
